4dd71df1b91e5a50ed4dfd913e4422740cb5bfef
Mrdrakert <57072568+Mrdrakert@users.noreply.github.com>
2024-09-28 21:35:28 +0200

    Alleluja alleluja alleluja alleeeeluuuuja

--- achievo/lib/animation.dart
+++ achievo/lib/animation.dart
@@ -4,33 +4,23 @@ import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
 
 class AnimatedImageSequence extends StatefulWidget {
-  final String animationName;  // Nazwa animacji (np. "skeleton")
-  final String initialState;   // Początkowy stan animacji (np. "idle")
-  final List<String> availableStates; // Lista dostępnych stanów (np. ["idle", "attack"])
-  final double? size; // Opcjonalna wielkość animacji
+  final String animationName;
+  final String initialState;
+  final List<String> availableStates;
+  final double? size;
+  final StreamController<String> stateController;
 
   const AnimatedImageSequence({
     Key? key,
     required this.animationName,
     required this.initialState,
     required this.availableStates,
+    required this.stateController,
     this.size,
   }) : super(key: key);
 
   @override
   _AnimatedImageSequenceState createState() => _AnimatedImageSequenceState();
-
-  // Nowa metoda do ustawiania stanu z zewnątrz
-  static _AnimatedImageSequenceState? of(BuildContext context) {
-    return context.findAncestorStateOfType<_AnimatedImageSequenceState>();
-  }
-
-  void setStateFromOutside(BuildContext context, String newState) {
-    final state = of(context);
-    if (state != null && availableStates.contains(newState)) {
-      state.setStateFromOutside(newState);
-    }
-  }
 }
 
 class _AnimatedImageSequenceState extends State<AnimatedImageSequence> {
@@ -38,17 +28,32 @@ class _AnimatedImageSequenceState extends State<AnimatedImageSequence> {
   late Timer _timer;
   String currentState = '';
   Map<String, List<String>> frames = {};
+  
+  // Flaga, czy wszystkie klatki zostały załadowane
+  bool areFramesFullyLoaded = false;
 
   @override
   void initState() {
     super.initState();
     currentState = widget.initialState;
-    _loadFrames().then((_) {
-      _startAnimation();
+
+    // Ładujemy i cache'ujemy wszystkie klatki na starcie
+    _loadAndCacheAllFrames().then((_) {
+      setState(() {
+        areFramesFullyLoaded = true; // Wszystkie klatki są gotowe
+        _startAnimation(); // Dopiero teraz startujemy animację
+      });
+    });
+
+    // Nasłuchujemy zmian stanu
+    widget.stateController.stream.listen((newState) {
+      if (areFramesFullyLoaded) {
+        _swapState(newState);
+      }
     });
   }
 
-  Future<void> _loadFrames() async {
+  Future<void> _loadAndCacheAllFrames() async {
     final basePath = 'animations/${widget.animationName}/';
     final manifestContent = await rootBundle.loadString('AssetManifest.json');
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
@@ -60,11 +65,28 @@ class _AnimatedImageSequenceState extends State<AnimatedImageSequence> {
           .toList();
 
       stateFrames.sort();
-      print('Wczytano ${stateFrames.length} klatek dla stanu $state');
       frames[state] = stateFrames;
-    }
 
-    setState(() {});
+      // Cache'owanie obrazów
+      for (String frame in stateFrames) {
+        await _cacheImage(frame);
+      }
+      print('Wczytano i zcache\'owano ${stateFrames.length} klatek dla stanu $state');
+    }
+  }
+
+  Future<void> _cacheImage(String assetPath) async {
+    // Ładujemy obraz z assets i cache'ujemy go
+    final ImageStream stream = AssetImage(assetPath).resolve(const ImageConfiguration());
+    final Completer<void> completer = Completer<void>();
+    final ImageStreamListener listener = ImageStreamListener((_, __) {
+      completer.complete();
+    }, onError: (dynamic exception, StackTrace? stackTrace) {
+      completer.completeError(exception, stackTrace);
+    });
+    stream.addListener(listener);
+    await completer.future;
+    stream.removeListener(listener);
   }
 
   void _startAnimation() {
@@ -76,13 +98,20 @@ class _AnimatedImageSequenceState extends State<AnimatedImageSequence> {
     });
   }
 
-  void setStateFromOutside(String newState) {
-    _timer.cancel();
-    setState(() {
-      currentState = newState;
-      _currentFrame = 0;
+  void _swapState(String newState) {
+    if (widget.availableStates.contains(newState) && newState != currentState) {
+      // Zatrzymaj aktualną animację
+      _timer.cancel();
+
+      // Zmień stan i zresetuj klatkę
+      setState(() {
+        currentState = newState;
+        _currentFrame = 0; // Resetujemy klatkę
+      });
+
+      // Uruchamiamy nową animację
       _startAnimation();
-    });
+    }
   }
 
   @override
@@ -94,10 +123,10 @@ class _AnimatedImageSequenceState extends State<AnimatedImageSequence> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: widget.size, // Ustawienie szerokości kontenera
-      height: widget.size, // Ustawienie wysokości kontenera
-      alignment: Alignment.center, // Wyśrodkowanie obrazu
-      child: frames.isEmpty || frames[currentState] == null || frames[currentState]!.isEmpty
+      width: widget.size,
+      height: widget.size,
+      alignment: Alignment.center,
+      child: !areFramesFullyLoaded
           ? CircularProgressIndicator()
           : Image.asset(
               frames[currentState]![_currentFrame],
--- achievo/lib/main.dart
+++ achievo/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:achievo/animation.dart';
 import 'package:flutter/material.dart';
 import 'package:nes_ui/nes_ui.dart';
-import 'package:achievo/main_app.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -9,32 +9,40 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  static const AnimatedImageSequence animatedImageSequence = AnimatedImageSequence(
-          animationName: 'skeleton',
-          initialState: 'idle',
-          availableStates: ['idle', 'attack'],
-          size: 300,
-        );
 
   @override
   Widget build(BuildContext context) {
+    // Tworzymy StreamController
+    final StreamController<String> stateController = StreamController<String>();
+
     return MaterialApp(
       title: 'Achievo',
       theme: flutterNesTheme(),
-      //home: const MainApp(),
       home: Column(
-      children: [
-        animatedImageSequence,
-        ElevatedButton(
-          onPressed: () {
-            AnimatedImageSequence.of(context)?.setStateFromOutside('attack');
-          },
-          child: Text('Przejdź do ataku'),
-        ),
-      ],
-      )
+        children: [
+          AnimatedImageSequence(
+            animationName: 'skeleton',
+            initialState: 'idle',
+            availableStates: ['idle', 'attack'],
+            stateController: stateController, // Przekazujemy kontroler
+            size: 300,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Zamiana stanu animacji
+              stateController.add('attack');
+            },
+            child: Text('Przejdź do ataku'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Zamiana stanu animacji z powrotem do 'idle'
+              stateController.add('idle');
+            },
+            child: Text('Przejdź do idle'),
+          ),
+        ],
+      ),
     );
   }
 }
-
-
