c9a004f3f0ec0e47d732b3a1484f382486fbb12a
kubsnn <51138889+kubsnn@users.noreply.github.com>
2024-09-28 19:43:45 +0200

    GoogleFit API for Android App

---
+++ achievo-api/src/config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    CLIENT_ID = '790013242756-4tts91qogos5abb1oq3krq7ch6nujjon.apps.googleusercontent.com'
+    CLIENT_SECRET = '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'
+    CREDENTIALS_FILE = '.google_fit_credentials'
