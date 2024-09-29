2d680e688ac76ebb5ed99dbab29d595ed98a3a2a
Krabbens <s188660@student.pg.edu.pl>
2024-09-29 02:42:45 +0200

    Add xp and gold fields to UserData model

--- achievo/lib/models/user_data.dart
+++ achievo/lib/models/user_data.dart
@@ -4,12 +4,16 @@ class UserData {
   final int weight;
   final int calories;
   final int sleep;
+  final int xp;
+  final int gold;
 
   UserData({
     required this.steps,
     required this.weight,
     required this.calories,
     required this.sleep,
+    required this.xp,
+    required this.gold,
   });
 
   factory UserData.fromJson(Map<String, dynamic> json) {
@@ -18,6 +22,8 @@ class UserData {
       weight: json['weight'] ?? 0,
       calories: json['calories'] ?? 0,
       sleep: json['sleep'] ?? 0,
+      xp: json['xp'] ?? 0,
+      gold: json['gold'] ?? 0,
     );
   }
 
@@ -27,6 +33,8 @@ class UserData {
       'weight': weight,
       'calories': calories,
       'sleep': sleep,
+      'xp': xp,
+      'gold': gold,
     };
   }
 }
